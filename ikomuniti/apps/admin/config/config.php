d88e04e0be616dd070e970764b65b83f5939c8d4
Azrul Haris <azrulharis85@gmail.com>
2014-05-28 20:46:30 +0800

    iShare

--- ikomuniti/apps/admin/config/config.php
+++ ikomuniti/apps/admin/config/config.php
@@ -4,8 +4,8 @@ return new \Phalcon\Config(array(
 	'database' => array(
 		'adapter'  => 'Mysql',
 		'host'     => '127.0.0.1',
-		'username' => 'ishareuser',
-		'password' => 'rahsiajun0137831318',
-		'name'     => 'ishare'
+		'username' => 'username',
+		'password' => 'uiuer84w95948',
+		'name'     => 'yewroiuey8998529'
 	)
 ));
--- ikomuniti/apps/frontend/config/config.php
+++ ikomuniti/apps/frontend/config/config.php
@@ -4,8 +4,8 @@ return new \Phalcon\Config(array(
 	'database' => array(
 		'adapter'  => 'Mysql',
 		'host'     => '127.0.0.1',
-		'username' => 'ishareuser',
-		'password' => 'rahsiajun0137831318',
-		'name'     => 'ishare'
+		'username' => 'dshtshsdfh',
+		'password' => '357537347gfgsd',
+		'name'     => 'dsfhdfh'
 	)
 ));
